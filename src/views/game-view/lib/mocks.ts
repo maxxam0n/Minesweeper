88d2e8ad4dd6ac0ce8bb02dc38c99ba9a753926b
Максим Коровин <maxxam0n@MacBook-Air-Maksim.local>
2025-06-28 13:16:37 +0300

    feat: Solver работает не стабильно, перехожу пока к разработке других полей

--- src/views/game-view/lib/mocks.ts
+++ src/views/game-view/lib/mocks.ts
@@ -1,9 +1,54 @@
 import { CellData } from '@/engine'
 
 export const mockField: CellData[][] = [
-	[number(0, 0, 3), number(1, 0, 4), number(2, 0, 3)],
-	[hidden(0, 1, true), hidden(1, 1), hidden(2, 1, true)],
-	[number(0, 2, 2), number(1, 2, 4), number(2, 2, 3)],
+	[
+		empty(0, 0),
+		empty(1, 0),
+		empty(2, 0),
+		empty(3, 0),
+		empty(4, 0),
+		empty(5, 0),
+	],
+	[
+		empty(0, 1),
+		number(1, 1, 1),
+		number(2, 1, 2),
+		number(3, 1, 2),
+		number(4, 1, 1),
+		empty(5, 1),
+	],
+	[
+		empty(0, 2),
+		hidden(1, 2),
+		hidden(2, 2),
+		hidden(3, 2),
+		hidden(4, 2),
+		empty(5, 2),
+	],
+	[
+		empty(0, 3),
+		hidden(1, 3),
+		hidden(2, 3),
+		hidden(3, 3),
+		hidden(4, 3),
+		empty(5, 3),
+	],
+	[
+		empty(0, 4),
+		number(1, 4, 1),
+		number(2, 4, 1),
+		number(3, 4, 2),
+		number(4, 4, 1),
+		empty(5, 4),
+	],
+	[
+		empty(0, 5),
+		empty(1, 5),
+		empty(2, 5),
+		empty(3, 5),
+		empty(4, 5),
+		empty(5, 5),
+	],
 ]
 
 function hidden(col: number, row: number, mine = false): CellData {
