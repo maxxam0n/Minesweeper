1fd9b32bbdca765ef76958d7bc1b78aff2a66a5f
Maxim Korovin <maximxam0n@gmail.com>
2025-06-29 00:38:57 +0300

    refactor: Переименовал сущности полей

--- src/engine/model/field-factory.ts
+++ src/engine/model/field-factory.ts
@@ -1,13 +1,21 @@
+import { HexagonalField } from './hexagonal-field'
 import { SquareField } from './square-field'
 import { FactoryConfig } from './types'
 
 export class FieldFactory {
 	static create(config: FactoryConfig) {
 		switch (config.type) {
-			default:
+			case 'hexagonal': {
+				const hexagonalField = new HexagonalField(config)
+				hexagonalField.placeMines()
+				return hexagonalField
+			}
+			case 'square':
+			default: {
 				const squareField = new SquareField(config)
 				squareField.placeMines()
 				return squareField
+			}
 		}
 	}
 }
